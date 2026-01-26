02a726ba326152dd20bf381dacd268cc8fcce0cc
Dmytro <dmytro.khudiyash@gmail.com>
2026-01-26 12:57:45 +0200

    Fix base path configuration for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import svgLoader from 'vite-svg-loader'
 
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [vue(), svgLoader()],
-  base: process.env.NODE_ENV === 'production' ? '/speed-typing/' : '/',
-})
+  base: mode === 'production' ? '/speed-typing/' : '/',
+}))
